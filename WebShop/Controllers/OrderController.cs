78ed9aba7511fbe9ab25e6af66dd4ecd17d59558
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-04-29 13:30:44 +0200

    changed Orders view so that user of the mvc site cannot enter in OrderStatus and TotalPrice for a order because these fields will be self generated

--- WebShop/Controllers/OrderController.cs
+++ WebShop/Controllers/OrderController.cs
@@ -50,7 +50,7 @@ namespace WebShop.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "OrderID,OrderStatus,ShippingDate,TotalPrice,CustomerID,ShippingCompanyID")] Order order)
+        public ActionResult Create([Bind(Include = "OrderID,ShippingDate,CustomerID,ShippingCompanyID")] Order order)   //removed orderstatus and totalprice from the bind include
         {
             if (ModelState.IsValid)
             {
