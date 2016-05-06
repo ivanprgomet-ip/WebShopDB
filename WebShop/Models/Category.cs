0ea2b4c22208754574c8860e1ebc74e03e0e5e01
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-05-06 12:04:42 +0200

    updating dataannotations in model classes

--- WebShop/Models/Category.cs
+++ WebShop/Models/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +9,11 @@ namespace WebShop.Models
 {
     public class Category
     {
+        //primary key
         public int CategoryID { get; set; }
+        [Required(ErrorMessage = "Category name is required")]
         public string CategoryName { get; set; }
+
         public string Description { get; set; }
     }
 }
--- WebShop/Models/Customer.cs
+++ WebShop/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,7 +11,10 @@ namespace WebShop.Models
         public int CustomerID { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public string MailAdress { get; set; }
+
+        [Required(ErrorMessage ="Email Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAdress { get; set; }
         public string StreetAdress { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
